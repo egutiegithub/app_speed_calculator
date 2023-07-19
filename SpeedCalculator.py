849a8c38c9f58a84db0163e14e668754bf3ecf90
egutie <egutie@gmail.com>
2023-07-18 20:31:54 -0600

    Initial Commit

---
+++ SpeedCalculator.py
@@ -0,0 +1,46 @@
+from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, QLineEdit, QPushButton, QComboBox
+import sys
+
+class SpeedCalculator(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Speed Calculator")
+        grid = QGridLayout()
+
+        distance_label = QLabel("Distance:")
+        self.distance_line_edit = QLineEdit()
+
+        time_label = QLabel("Time (hours):")
+        self.time_line_edit = QLineEdit()
+
+        calculate_button = QPushButton("Calculate")
+        calculate_button.clicked.connect(self.calculate_speed)
+        self.output_label = QLabel("")
+
+        self.unit_combo = QComboBox()
+        self.unit_combo.addItems(["Metric (Km)", "Imperial (Miles)"])
+
+        grid.addWidget(distance_label, 0, 0)
+        grid.addWidget(self.distance_line_edit, 0, 1)
+        grid.addWidget(self.unit_combo, 0, 2)
+        grid.addWidget(time_label, 1, 0)
+        grid.addWidget(self.time_line_edit, 1, 1)
+        grid.addWidget(calculate_button,2, 1, 1, 1)
+        grid.addWidget(self.output_label, 3, 0, 1, 2)
+
+        self.setLayout(grid)
+
+    def calculate_speed(self):
+        if self.unit_combo.currentText() == "Metric (Km)":
+            unit = "km/h"
+        else:
+            unit = "mph"
+
+        speed = float(self.distance_line_edit.text()) / float(self.time_line_edit.text())
+        self.output_label.setText(f"Average speed: {speed} {unit}")
+
+
+app = QApplication(sys.argv)
+speed_calculator = SpeedCalculator()
+speed_calculator.show()
+sys.exit(app.exec())
